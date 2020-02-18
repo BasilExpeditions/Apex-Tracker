4549672b20fe483cf5fc646594cccf87a15cbe95
basil <basilexpeditions@gmail.com>
2020-02-18 14:42:28 +1100

    Setup Node backend, Get working with apex-legends-api, pre react installation

---
+++ server.js
@@ -0,0 +1,36 @@
+//
+const express = require('express');
+
+//
+const morgan = require('morgan');
+
+//
+const dotenv = require('dotenv');
+
+//Load config.env file(Containing Variables)
+dotenv.config({ path: './config.env' });
+
+const app = express();
+
+//Dev logging - Allows log messages in console such as:
+//"GET /api/v1/profile/psn/Soviet_Monkey- 200 25.385 ms - 5"
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+//Profile routes
+app.use('/api/v2/profile', require('./routes/profile'));
+
+//Running on port 5000 else running on port 8000
+const port = process.env.PORT || 8000;
+
+//Listening to port
+app.listen(port, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} port ${port}`)
+);
+
+//TRN-Api-Key: XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
+
+//app.get('/', (req, res) => res.send('Hello World!'));
+
+//Get request on APEX LEGENDS API URL
