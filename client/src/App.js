d124dfec9f62cfcf569ecbf0d3134b8ef90abb21
basil <basilexpeditions@gmail.com>
2020-03-03 12:21:29 +1100

    Added About, Home & PlayerStats pages via react-routing, Need to fix state for onSubmit in State.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,50 @@
 import React from 'react';
-import Header from './components/header';
+import PlayerStats from './pages/playerStats';
+import About from './pages/about';
+import Home from './pages/home';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className='container'>
-      <Header />
-      Changes Saved
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/playerStats'>Player Stats</Link>
+            </li>
+            <li>
+              <Link to='/about'>About</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path='/about'>
+            <About />
+          </Route>
+          <Route path='/playerStats'>
+            <PlayerStats />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+
+    //const App = () => {
+    //return (
+    //<div className='container'>
+    //<Header />
+    //Changes Saved
+    //</div>
   );
 };
 
